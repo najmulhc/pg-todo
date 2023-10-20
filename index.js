b51f2fbc0df55389841028d84145f1953b8d54b2
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-10-20 18:58:55 +0600

    created post and delete request handler for todo requests

--- index.js
+++ index.js
@@ -5,26 +5,53 @@ const pool = require("./db");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
+
+app.post("/", async (req, res) => {
+  try {
+    const { name } = req.body;
+    const result = await pool.query(
+      `INSERT  INTO todos (name) VALUES ('${name}'); 
+      SELECT * FROM todos
+      `
+    );
+
+    res.json({
+      todos: result[1].rows,
+    });
+  } catch (error) {
+    res.json(error.message);
+  }
+});
 
 app.get("/", async (req, res) => {
-  pool.query("SELECT * FROM todos;", (error, results) => {
-    if(error) {
-        res.json({
-            ...error
-        })
-    } else {
-     const todos = results.rows;
-     res.json({
-        todos
-     })
-    }
-  })
-})
+  const result = await pool.query("SELECT * FROM todos;");
+  const todos = result.rows;
+  res.json({
+    todos,
+  });
+});
 
+
+app.delete("/", async (req, res) => {
+  try {
+    const { id } = req.query;
+
+    const result = await pool.query(
+      `DELETE FROM todos WHERE id=${parseInt(id)}; SELECT * FROM todos;`
+    );
+    const output = await pool.query("");
+    res.json({
+      todos: result[1].rows,
+    });
+  } catch (error) {
+    res.json(error.message);
+  }
+});
 
 const port = process.env.PORT || 5500;
 
 app.listen(port, () => {
-    console.log(`The express app is running in the port ${port}`)
-})
+  console.log(`The express app is running in the port ${port}`);
+});
